fa300e505eb7ac097cfdee6cecff22716206d58f
Allyssonrenzo <130418629+Allyssonrenzo@users.noreply.github.com>
2023-07-11 11:22:49 -0300

    Add files via upload

---
+++ sac-if/cadaluno.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+   
+  <form method="GET" action="crudaluno.php">
+    <label for="">nome aluno</label>
+     <input type="text" name="nome">
+     <label for="">idade</label>
+     <input type="text" name="idade"> 
+     <label for="">turma</label>
+     <input type="text" name="turma">
+     <label for="">endereço</label>
+     <input type="text" name="endereco"> 
+     <label for="">data de nascimento</label>
+     <input type="text" name="datanas">
+     
+
+     <input type="submit" name="cadastrar" value="cadastrar">
+     </form>
+
+     <button class="button"><a href="index.php">voltar</a></button>
+
+</body>
+</html>
---
+++ sac-if/index.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+        <h1>SAC-IF BAHIANO</h1>
+        <p>Seja bem vindo!</p>
+      </div>
+      
+      
+      <div class="container mt-3">
+      <h1> Realise o cadastro de alunos:</h1>
+      <button type="button" class="btn btn-dark" style="margin-left: 5px;"><a href="cadaluno.php">Cadastrar</a></button>
+      <h1> Lista de alunos:</h1>
+      <button type="button" class="btn btn-dark" style="margin-left: 5px; "><a href="listaalunos.php">Lista</a></button>
+     
+  
+      </div>
+    </div>
+
+
+</body>
+</html>
